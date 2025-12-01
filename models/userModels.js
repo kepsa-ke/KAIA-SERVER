f7ae72538fe0c7479f612650031ea11ee18f35e0
gitkraken303 <johnspaxxvile@gmail.com>
2025-12-01 15:13:11 +0300

    fixing the same issue

--- models/userModels.js
+++ models/userModels.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-    },
     email: {
       type: String,
       required: true,
